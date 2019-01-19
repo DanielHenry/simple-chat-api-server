c2bff3ecb6ac8b605130d31be88db0a37e094a2f
DanielHenry <daniel.henry.39@gmail.com>
2019-01-20 06:38:31 +0700

    Add API url

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+Route::group('api', function () {
+    Route::post('send-message', 'ChatController@sendMessage');
+    Route::post('get-messages', 'ChatController@getMessages');
 });
