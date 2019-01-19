b1d4f716a48f7edda9c17eba1d4c174c6ba5bbc5
DanielHenry <daniel.henry.39@gmail.com>
2019-01-20 04:51:48 +0700

    Add template for validation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,31 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+use Validator;
+use App\Http\ApiResponder;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ApiResponder;
+
+    protected function customValidation(Request $request, $rules = [], $messages = [])
+    {
+        $validator = Validator::make($request->all(), $rules, $messages);
+        if ($validator->fails()) {
+            return $this->invalidParameters($validator->errors()->all());
+        } else {
+            return TRUE;
+        }
+    }
+
+    protected function guardWithValidation(Request $request, $rules = [], $messages = [], $callback)
+    {
+        $validator = Validator::make($request->all(), $rules, $messages);
+        if ($validator->fails()) {
+            return $this->invalidParameters($validator->errors()->all());
+        } else {
+            return $callback();
+        }
+    }
 }
